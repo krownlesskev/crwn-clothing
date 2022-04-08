1bd93ac061a766c5100ea1a1dcf7d9835ba729be
Kevin Sana <kevin.sana@outlook.com>
2022-04-08 13:55:09 -0500

    matching code with ref

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,14 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../../utils/firebase/firebase.utils'
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+} from '../../../utils/firebase/firebase.utils'
+
+
+
 const Signin = () => {
     const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup();
-        createUserDocumentFromAuth(user);
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
     }
 
     return (

13714079d54efbb953c79df52497aa1ffca26039
Kevin Sana <kevin.sana@outlook.com>
2022-04-08 12:39:11 -0500

    setting up user docs for db

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,3 +1,4 @@
+import { async } from '@firebase/util';
 import { initializeApp } from 'firebase/app';
 import {
     getAuth,
@@ -5,6 +6,13 @@ import {
     signInWithPopup,
     GoogleAuthProvider
 } from 'firebase/auth'
+
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBkQlw2XsS2s_Z9KyLAHaKSLAhCRvav2Ck",
@@ -25,3 +33,15 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot)
+    console.log(userSnapshot.exists())
+}

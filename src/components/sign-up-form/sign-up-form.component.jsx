5b7994a8b0a9e758eb258fdbc80ec16af142a901
Kevin Sana <kevin.sana@outlook.com>
2022-04-12 01:47:15 -0500

    created form-input component and styled

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 
+import FormInput from "../form-input/form-input.component";
+
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
 const defaultFormFields = {
@@ -50,32 +52,32 @@ const SignUpForm = () => {
         <div>
             <h1>Sign up with your email and password</h1>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="">Display Name</label>
-                <input
+                <FormInput
+                    label="Display Name"
                     type="text"
                     required
                     onChange={handleChange}
                     name='displayName'
                     value={displayName} />
 
-                <label htmlFor="">Email</label>
-                <input
+                <FormInput
+                    label="Email"
                     type="email"
                     required
                     onChange={handleChange}
                     name='email'
                     value={email} />
 
-                <label htmlFor="">Password</label>
-                <input
+                <FormInput
+                    label="Password"
                     type="password"
                     required
                     onChange={handleChange}
                     name='password'
                     value={password} />
 
-                <label htmlFor="">Confirm Password</label>
-                <input
+                <FormInput
+                    label="Confirm Password"
                     type="password"
                     required
                     onChange={handleChange}

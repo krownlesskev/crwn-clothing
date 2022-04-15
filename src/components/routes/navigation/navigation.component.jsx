558f4a32d5930d2ec45fa27187ea6b2655362741
Kevin Sana <kevin.sana@outlook.com>
2022-04-15 02:18:58 -0500

    added userContext and implemented useState

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,8 +1,14 @@
+import userEvent from "@testing-library/user-event"
+import { useContext } from "react"
 import { Outlet, Link } from "react-router-dom"
 import { ReactComponent as CrwnLogo } from '../../../assets/crown.svg'
+import { UserContext } from "../../../context/user.context"
 import './navigation.styles.scss'
 
 const Navigation = () => {
+    const { currentUser } = useContext(UserContext);
+    console.log(currentUser)
+
     return (
         <>
             <div className="navigation">
